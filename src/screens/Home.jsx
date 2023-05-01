dfe699aa31848925a45eb50845a92511035aa2ac
Caio AReis <Caio.almeida16.ca@gmail.com>
2023-05-01 20:12:31 -0300

    Pegando o usuário atual do contexto na Home

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,15 +1,34 @@
+import { useContext } from "react";
 import { Ionicons } from "@expo/vector-icons";
+import { SessionContext } from "../config/SessionContext";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Avatar, Box, Fab, HStack, Heading, Icon, Text } from "native-base";
+import { Avatar, Box, Fab, HStack, Heading, Icon, Pressable, Text } from "native-base";
 
 export function Home({ navigation }) {
+
+  const [ session, setSession ] = useContext(SessionContext);
+
+  const handleLogOut = () => {
+    setSession(null);
+    navigation.navigate("SignIn");
+  }
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <Box flex={1}>
-        <HStack alignItems="center" space={3} p={3}>
-          <Avatar bg="danger.600">CA</Avatar>
-          <Text>Caio AReis</Text>
+        <HStack alignItems="center" justifyContent="space-between" p={3}>
+          <HStack space={3} alignItems="center">
+            <Avatar bg="danger.600">{session?.name[ 0 ]}</Avatar>
+            <Text>{session?.name}</Text>
+          </HStack>
+
+          <Pressable onPress={handleLogOut}>
+            <Icon
+              mr={3} size={8}
+              color="danger.500"
+              as={<Ionicons name="log-out-outline" />}
+            />
+          </Pressable>
         </HStack>
 
         <Heading textAlign="center" mt={20} color="gray.700">Welcome to APP</Heading>
